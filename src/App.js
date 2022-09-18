9d17501e6973ada779ade73fb9176d9662ff04ba
johntarus <tarusjohn96@gmail.com>
2022-09-18 12:23:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  const activeMenu = false;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative">
+          {activeMenu ? (
+            <div className="fixed bg-lightBlue w-60 min-h-screen rounded-r-lg text-white">
+              Sidebar1
+            </div>
+          ) : (
+            <div className="w-0">Sidebar</div>
+          )}
+          <div
+            className={`bg-mainBg min-h-screen w-full ${
+              activeMenu ? "md:ml-0" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-Bg navbar w-full shadow-md h-20">
+              Navbar
+            </div>
+          </div>
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element="Home" />
+              <Route path="/inventory" element="Inventory" />
+              <Route path="/transactions" element="Transactions" />
+              <Route path="/people" element="People" />
+              <Route path="/shops" element="Shops" />
+              <Route path="/chart-support" element="Chart Support" />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
