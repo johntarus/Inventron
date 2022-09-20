f4731dfbbd298cdf16b6f1084683f5f6e208827b
johntarus <tarusjohn96@gmail.com>
2022-09-20 23:20:31 +0300

    feat: added desktop sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import home from '../assets/home.svg'
 import { MdOutlineCancel } from 'react-icons/md'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import { dashBoardLinks } from '../assets/data'
+import profile from '../assets/profile.jpg'
+import { RiArrowDropDownLine } from 'react-icons/ri'
+import { useStateContext } from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-    const activeMenu = true
+    const { activeMenu, setActiveMenu, screenSize } = useStateContext()
+
+    const handleCloseSideBar = () => {
+        if (activeMenu && screenSize < 900) {
+            setActiveMenu(false)
+        }
+    }
+
     const activeLink = `flex items-center gap-4 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2`
     const normalLink = `flex items-center gap-4 pl-4 pt-3 pb-2.5 rounded-lg text-md text-white hover:text-gray-300 m-2`
     return (
@@ -16,18 +25,22 @@ const Sidebar = () => {
                     <div className="flex justify-between items-center">
                         <Link
                             to="/home"
-                            onClick={() => {}}
+                            onClick={handleCloseSideBar}
                             className="items-center gap-3 ml-3 pl-4 mt-4 flex text-3xl font-extrabold text-[#FFE6C9]"
                         >
                             Inventron
                         </Link>
                         <TooltipComponent
-                            content="menu"
+                            content="Menu"
                             position="BottomCenter"
                         >
                             <button
                                 type="button"
-                                onClick={() => {}}
+                                onClick={() =>
+                                    setActiveMenu(
+                                        (prevActiveMenu) => !prevActiveMenu
+                                    )
+                                }
                                 className="text-xl rounded-full p-3 mt-4 md:hidden block"
                             >
                                 <MdOutlineCancel />
@@ -39,7 +52,7 @@ const Sidebar = () => {
                             <NavLink
                                 to={`/${link.linkName}`}
                                 key={link.name}
-                                onClick={() => {}}
+                                onClick={handleCloseSideBar}
                                 className={({ isActive }) =>
                                     isActive ? activeLink : normalLink
                                 }
@@ -52,6 +65,25 @@ const Sidebar = () => {
                                 <span>{link.name}</span>
                             </NavLink>
                         ))}
+                    </div>
+                    <div className="flex mt-12">
+                        <img
+                            src={profile}
+                            className="h-8 w-8 ml-8 rounded-full"
+                        />
+                        <div className="flex flex-col text-xs ml-2">
+                            <p>Tafari Mwangi</p>
+                            <p className="flex items-center">
+                                Active
+                                <span>
+                                    {
+                                        <RiArrowDropDownLine
+                                            style={{ fontSize: '14px' }}
+                                        />
+                                    }
+                                </span>
+                            </p>
+                        </div>
                     </div>
                 </>
             )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {
-            colors: { lightBlue: '#2951FE' },
+            colors: { lightBlue: '#2951FE', lightGray: 'gray-300' },
             backgroundColor: { mainBg: '#FFFFFF' },
             height: { maxHeight: '1080px' },
             margin: { marginLeft: 'ml-16' },
