9711ab2acba4a5e299d723a2eab1a823e084cee4
tarus <tarusjohn96@gmail.com>
2022-09-25 10:04:03 +0300

    refactor: refactored home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,25 +3,6 @@ import hero from '../assets/hero.svg'
 
 const Home = () => {
     return (
-        // <div>
-        //     <div className="font-bold mt-16 ml-2 md:ml-0 md:mt-2 text-xl text-gray-700">
-        //         Dashboard
-        //     </div>
-        //     <div className="md:flex bg-white w-full justify-between rounded-xl lg:w-80 p-8 pt-9 m-3">
-        //         <div className="m-0">
-        //             <h1>Welcome back Tafari!</h1>
-        //             <ul className="list-disc marker:text-[#FD9C29]">
-        //                 <li>21 new sales</li>
-        //             </ul>
-        //             <ul className="list-disc marker:text-[#FD9C29]">
-        //                 <li>15 new restocked items</li>
-        //             </ul>
-        //             <ul className="list-disc marker:text-[#FD9C29]">
-        //                 <li>45 new pending credit sales reminders</li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </div>
         <div className="mt-16 ml-0 md:mt-0 rounded-xl md:w-4/5">
             <div className="my-2 font-bold text-xl">Dashboard</div>
             <div className="flex lg:flex-nowrap shadow-xl rounded-xl">
