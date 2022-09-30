539f9e15d64fa9dfdff0874baa161d8f485e8853
tarus <tarusjohn96@gmail.com>
2022-09-30 17:32:37 +0300

    ref: refactored the bar chart component

---
+++ src/components/charts/BarChart.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2'
+import { Chart as ChartJS } from 'chart.js/auto'
+
+const BigBarChart = () => {
+    const data = {
+        labels: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'June',
+            'July',
+            'Aug',
+            'Sept',
+            'Oct',
+            'Nov',
+            'Dec',
+        ],
+        datasets: [
+            {
+                label: 'Product Performance',
+                data: [20, 40, 37, 20, 35, 45, 33, 45, 43, 45, 43, 48],
+                barThickness: 30,
+            },
+        ],
+    }
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                grid: {
+                    display: false,
+                },
+            },
+            y: {
+                grid: {
+                    display: false,
+                },
+                ticks: {
+                    display: false,
+                },
+            },
+        },
+    }
+    return (
+        <div>
+            <div className="flex justify-between m-1 mb-4">
+                <div className="flex items-center font-bold">
+                    <h1 className="font-bold">Product Performance</h1>
+                </div>
+                <button className="bg-lightBlue hover:bg-blue-700 text-white py-0.5 px-6 rounded-full">
+                    view all
+                </button>
+            </div>
+            <div className="flex items-center gap-1 md:gap-12 m-1 text-xs font-bold">
+                <h1 className="performing text-lightBlue thick">
+                    Best Performing Product
+                </h1>
+                <h1>Average Perfoming Product</h1>
+                <h1>Worst Perfoming Product</h1>
+            </div>
+            <div className="shadow-xl rounded-xl">
+                <Bar data={data} options={options} height="200px" />
+            </div>
+        </div>
+    )
+}
+
+export default BigBarChart
