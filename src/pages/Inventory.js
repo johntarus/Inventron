889d2f8225b7f1252d7bbe28bbce9afa2fce8989
tarus <tarusjohn96@gmail.com>
2022-10-04 11:59:39 +0300

    feat: pages content

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Inventory = () => {
-    return <div>Inventory</div>
+    return (
+        <div className="mt-16 mx-4 md:ml-0 md:px-8">
+            <p>This page has not been implemented yet!.</p>
+        </div>
+    )
 }
 
 export default Inventory
--- src/pages/Shops.js
+++ src/pages/Shops.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Shops = () => {
-    return <div>Shops</div>
+    return (
+        <div className="mt-16 mx-4 md:ml-0 md:px-8">
+            <p>This page has not been implemented yet!.</p>
+        </div>
+    )
 }
 
 export default Shops
